1152ca8e05c870a021cc39918b6211ece564461b
Aerith <96123326+Katou77Megumin@users.noreply.github.com>
2022-01-25 16:09:40 +0800

    Update get_up.py

--- get_up.py
+++ get_up.py
@@ -81,7 +81,7 @@ def main(github_token, repo_name, weather_message, tele_token, tele_chat_id):
             )
     else:
         bodylate = body + f"\n 你今天起的还蛮“早”的，半天就酱紫过去咯\n" 
-         issue.create_comment(bodylate)
+        issue.create_comment(bodylate)
         # send to telegram
         if tele_token and tele_chat_id:
             requests.post(
